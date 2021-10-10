8af92f299f1ecadec32c324ee53a09d75cf3661d
jlopcun <62543437+jlopcun@users.noreply.github.com>
2021-10-10 20:58:19 +0200

    push

---
+++ main.js
@@ -0,0 +1,37 @@
+const cursor = document.getElementById('cursor');
+const RootStyles = document.documentElement.style;
+const cursorStyle = getComputedStyle(cursor)
+window.addEventListener('mousemove',(e)=>{
+    let x = e.clientX;
+    let y = e.clientY;
+    if (x+45<window.innerWidth) RootStyles.setProperty('--Xpos',`${x-25}px`);
+    if(y+45<window.innerHeight) RootStyles.setProperty('--Ypos',`${y-25}px`);
+})
+    
+console.log(window.innerHeight);
+
+
+let menuOpener = document.getElementById('menuOpener');
+let menuToOpen = document.getElementById('menuToOpen');
+menuOpener.addEventListener('click',()=>{
+    menuToOpen.classList.toggle('show');
+    document.body.classList.toggle('menuOpened');
+})
+
+let darklight = document.getElementById('darklight');
+
+darklight.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('fa-moon')){
+        e.target.classList.add('fa-sun');
+        e.target.classList.remove('fa-moon');
+        RootStyles.setProperty('--bodybg','#fff');
+        RootStyles.setProperty('--bodytxt','#000');
+    }
+    else{
+        e.target.classList.add('fa-moon'); 
+        e.target.classList.remove('fa-sun');
+        RootStyles.setProperty('--bodybg','#333');
+        RootStyles.setProperty('--bodytxt','#fff');
+    }
+    
+})
